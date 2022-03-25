91611356b913e424af59af661827dfbb27c2c1cb
이태수 <fugitive81@gmail.com>
2022-03-26 02:17:54 +0900

    init

---
+++ CloneFLO/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  CloneFLO
+//
+//  Created by TAE SU LEE on 2022/03/26.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
